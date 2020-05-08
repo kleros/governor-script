e9424ab80bd81c2aa51ddfd3f6228d389f56e5dd
Sam Vitello <sam.j.vitello@gmail.com>
2020-05-08 15:10:33 -0600

    governor script

--- src/index.js
+++ src/index.js
@@ -1,104 +1,37 @@
 const Web3 = require('web3')
-const rc_question = require('@realitio/realitio-lib/formatters/question.js');
-const rc_template = require('@realitio/realitio-lib/formatters/template.js');
-const _realitioProxy = require('../assets/realitio-proxy.json')
-const _realitioContract = require('../assets/realitio.json')
-
+const _governor = require('../assets/governor.json')
 // FILL THESE IN FOR THE NETWORK
 const web3URI = 'https://mainnet.infura.io/v3/668b3268d5b241b5bab5c6cb886e4c61'
-const realitioProxyContractAddress = '0x57adb6aa9edaa9abfd495b519d94d08f47321e26'
-const realitioContractAddress = '0x325a2e0F3CCA2ddbaeBB4DfC38Df8D19ca165b47'
+const governorAddress = '0x81dCc6246Fe261035FFeE91CD975FAf3D3f3375F'
 // Can add this for speed
-const fromBlock = 6531265
+const fromBlock = 14676408
 
 const getMetaEvidence = async () => {
   const web3 = new Web3(web3URI)
 
-  const proxyContractInstance = new web3.eth.Contract(
-    _realitioProxy.abi,
-    realitioProxyContractAddress
+  const governorInstance = new web3.eth.Contract(
+    _governor.abi,
+    governorAddress
   )
 
-  const realitioContractInstance = new web3.eth.Contract(
-    _realitioContract.abi,
-    realitioContractAddress
-  )
-
-  const realitioIDEventLog = await proxyContractInstance.getPastEvents(
-    'DisputeIDToQuestionID',
-    {
-      filter: {
-        _disputeID: scriptParameters.disputeID
-      },
-      fromBlock: fromBlock,
-      toBlock: 'latest'
-    }
-  )
-  const realitioID = realitioIDEventLog[0].returnValues._questionID
-
-  const questionEventLog = await realitioContractInstance.getPastEvents(
-    'LogNewQuestion',
-    {
-      filter: {
-        question_id: realitioID
-      },
-      fromBlock: fromBlock,
-      toBlock: 'latest'
-    }
-  )
-  const templateID = questionEventLog[0].returnValues.template_id
-
-  const templateEventLog = await realitioContractInstance.getPastEvents(
-    'LogNewTemplate',
-    {
-      filter: {
-        template_id: templateID
-      },
-      fromBlock: fromBlock,
-      toBlock: 'latest'
-    }
-  )
-  const templateText = templateEventLog[0].returnValues.question_text;
-  const questionData = rc_question.populatedJSONForTemplate(templateText, questionEventLog[0].returnValues.question)
-
-  switch (questionData.type) {
-    case 'bool':
-      resolveScript({
-        rulingOptions: {
-          type: 'single-select',
-          titles: ["No", "Yes"]
-        }
-      })
-    case 'uint':
-      resolveScript({
-        rulingOptions: {
-          type: 'uint',
-          precision: questionData["decimals"]
-        }
-      })
-    case 'single-select':
-      resolveScript({
-        rulingOptions: {
-          type: 'single-select',
-          titles: questionData.outcomes
-        }
-      })
-    case 'multiple-select':
-      resolveScript({
-        rulingOptions: {
-          type: 'multiple-select',
-          titles: questionData.outcomes
-        }
-      })
-    case 'datetime':
-      resolveScript({
-        rulingOptions: {
-          type: 'datetime'
-        }
-      })
-    default:
-      resolveScript({})
+  const _disputeID = scriptParameters.disputeID
+  let sessionNumber = await governorInstance.methods.getCurrentSessionNumber().call()
+  let session
+  while (sessionNumber >= 0) {
+    session = await governorInstance.methods.sessions(sessionNumber).call()
+    if (Number(session.disputeID) === Number(_disputeID)) break
+    sessionNumber--
   }
+
+  if (sessionNumber < 0) throw new Error("Unable to find disputed session")
+  const listIDs = await governorInstance.methods.getSubmittedLists(sessionNumber).call()
+  let titles = await Promise.all(listIDs.map(listID => 'List ' + listID))
+  resolveScript({
+    rulingOptions: {
+      type: 'single-select',
+      titles
+    }
+  })
 }
 
 module.exports = getMetaEvidence
